15bc749b8eb82801c9cfdfd4c4e62d135f005c87
Dominic Cork <dcork1@kent.edu>
2024-12-03 23:59:17 -0500

    Fix circular import

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,9 @@
 from flask import Flask
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
-from .models import db, User
 
 bcrypt = Bcrypt()
 login_manager = LoginManager()
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.get_or_none(User.id == int(user_id))  # Fetch user by ID
 
 def create_app():
     app = Flask(__name__)
@@ -16,16 +11,16 @@ def create_app():
     app.config['DATABASE'] = 'fitness_app.db'
 
     # Initialize extensions
-    db.init_app(app)
     bcrypt.init_app(app)
     login_manager.init_app(app)
-
-    # Set up login manager
     login_manager.login_view = 'auth.login'
 
-    # Register blueprints
+    # Import models and register blueprints here to prevent circular imports
+    from .models import db, User
+    db.init_app(app)
+
     from .routes import main
-    from .auth import auth  # Authentication blueprint
+    from .auth import auth
     app.register_blueprint(main)
     app.register_blueprint(auth, url_prefix='/auth')
 

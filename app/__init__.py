f6a6800cc18c87e3437c1b7774f02fb63827e574
Dominic Cork <dcork1@kent.edu>
2024-12-06 16:42:57 -0500

    Add additional comments for clarity

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
-from .models import db, User, Goal, Workout
+from .models import db, User
 from .routes import main
 import os
 
@@ -9,34 +9,34 @@ import os
 bcrypt = Bcrypt()
 login_manager = LoginManager()
 
+# Load user by ID for login session management
 @login_manager.user_loader
 def load_user(user_id):
-    # Load user by ID for login session management
-    return User.get_or_none(User.id == int(user_id))
+    return User.get_or_none(User.id == int(user_id))  
 
 # Create and configure the Flask app
-def create_app(): 
+def create_app():
     app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'your_secret_key'
 
     # Application configurations
-    app.config['SECRET_KEY'] = 'your_secret_key'
+    app.config['SESSION_PROTECTION'] = 'strong'  
+    app.config['REMEMBER_COOKIE_DURATION'] = 0  
+
+    
+    # Set the database path
     app.config['DATABASE'] = os.path.join(os.path.dirname(__file__), 'databases', 'fitness_app.db')
 
     # Initialize extensions
     bcrypt.init_app(app)
     login_manager.init_app(app)
+    login_manager.login_view = 'auth.login'
 
-    # Default view for unauthorized access
-    login_manager.login_view = 'auth.login' 
-
-    # Initialize database
+    # Initialize the database
     db.init(app.config['DATABASE'])
 
-    # Create tables if they don't already exist
-    with db:
-        db.create_tables([User, Goal, Workout])
-
-    # Register Blueprints
+    # Register blueprints
+    from .routes import main
     from .authenticate import auth
     app.register_blueprint(main)
     app.register_blueprint(auth, url_prefix='/auth')

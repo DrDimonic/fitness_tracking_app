fcec7560c682d59e57c9a18f186ee522de31e867
Dominic Cork <dcork1@kent.edu>
2024-12-06 18:32:29 -0500

    Remove test database

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from app import create_app
 # Create the application instance 
 app = create_app()
 
-# Run the application in debug mode
+# Run the application
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug = False)
